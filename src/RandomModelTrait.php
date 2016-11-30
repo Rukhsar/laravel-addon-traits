8307c95dbc4eafbc4f4100b3e6d0c88af5975dd4
Rukhsar <rukhsar.man@gmail.com>
2016-11-30 11:52:16 +0000

    random model trait added

--- src/RandomModelTrait.php
+++ src/RandomModelTrait.php
@@ -6,4 +6,47 @@ trait RandomModelTrait
 {
 
 
+    /**
+     * Scope a query to only one random item.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeRandom($query)
+    {
+        // Get total number of items returned by a query
+        $total = $query->count();
+
+        if ($total > 1)
+        {
+            $offset = mt_rand(0, $total - 1);
+            $query = $query->skip($offset)->take(1);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Return a random instance of model.
+     *
+     * @return static|null
+     */
+    public static function getRandom()
+    {
+        return static::random()->first();
+    }
+
+
+    /**
+     * Return a random instance of model or fail.
+     *
+     * @return static
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public static function getRandomOrFail()
+    {
+        return static::random()->firstOrFail();
+    }
 }
